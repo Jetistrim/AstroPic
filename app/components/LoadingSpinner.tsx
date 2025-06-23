e644b727cca93a5a0d52d3fd074a5470d7568648
Jetistrim <paulogabriellima@hotmail.com>
2025-06-23 16:34:03 -0300

    feat: apear random phrases on loading

--- app/components/LoadingSpinner.tsx
+++ app/components/LoadingSpinner.tsx
@@ -1,16 +1,23 @@
+import { useEffect, useState } from "react";
+
 export const LoadingSpinner = () => {
    const getRandomItem = (array: string[]) => {
       const randomIndex = Math.floor(Math.random() * array.length);
-      return array[randomIndex];
+      return array[randomIndex]
    }
+
+   const [phrase, setPhrase] = useState("");
+
+   useEffect(() => {
+      setPhrase(getRandomItem(phrases));
+   }, []);
+
    const phrases = ["A buscar as estrelas...", "Tirando foto de buraco negro...", "Guardando matéria escura..."]
 
    return (
       <div className="flex flex-col items-center justify-center gap-4 text-center">
          <span className="loading loading-spinner text-primary loading-lg"></span>
-         <p className="text-primary/70 text-lg">{
-            getRandomItem(phrases)
-         }</p>
+         <p className="text-primary/70 text-lg">{phrase}</p>
       </div>
    )
 }
